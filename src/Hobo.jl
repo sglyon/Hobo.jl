f24e5af029a0451424916c78f535f71cfd03032e
Spencer Lyon <spencerlyon2@gmail.com>
2015-06-15 10:56:04 -0400

    CLN: remove state space stuff

--- src/Hobo.jl
+++ src/Hobo.jl
@@ -8,12 +8,10 @@ import Base: +, mean, std, var, push!, copy
 import StatsBase: mean, std, var, skewness, kurtosis
 
 export
-    LinearGaussianSSabcd, kfilter, loglik,
     csminwel, RunningMoments, skewness, kurtosis, gensys
 
 include("bayes.jl")
 include("csminwel.jl")
-include("lin_gauss_ss.jl")
 include("gensys.jl")
 include("running_moments.jl")
 
